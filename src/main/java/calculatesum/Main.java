8a5d9f6fa0bb80ffb9ba6a7b950329931e29c3de
Andrewmazyar <maziarandriy@gmail.com>
2020-07-02 13:42:37 +0300

    fix maven problem

--- src/main/java/calculatesum/Main.java
+++ src/main/java/calculatesum/Main.java
@@ -15,19 +15,11 @@ public class Main {
         }
         List<ThreadCallable> threadCallables = new ArrayList<>();
         for (int i = 0; i < 1_000_000; i = i + 250_000) {
-            threadCallables.add( new ThreadCallable(integerList.subList(i, i + 250_000)));
+            threadCallables.add(new ThreadCallable(integerList.subList(i, i + 250_000)));
         }
         ExecutorService executorService = Executors.newFixedThreadPool(4);
-        List<Future<Integer>> futuresExecutor = executorService
-                .invokeAll(List.of(threadCallables.get(0),
-                        threadCallables.get(1),
-                        threadCallables.get(2),
-                        threadCallables.get(3)));
+        List<Future<Integer>> futuresExecutor = executorService.invokeAll(threadCallables);
         ForkJoinPool forkJoinPool = new ForkJoinPool(4);
-        List<Future<Integer>> futuresJoin = forkJoinPool
-                .invokeAll(List.of(threadCallables.get(0),
-                        threadCallables.get(1),
-                        threadCallables.get(2),
-                        threadCallables.get(3)));
+        List<Future<Integer>> futuresJoin = forkJoinPool.invokeAll(threadCallables);
     }
 }
